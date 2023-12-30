d6d0ecb902489c07624bc6c299f643f84abe0150
jerome-shiftleft <jerome@theshiftleft.com>
2023-12-31 03:17:45 +0800

    progress

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -2,7 +2,8 @@ import {createContext} from 'react';
 
 const CartContext = createContext({
   items: [],
-  addItemToCart: () => {}
+  addItemToCart: () => {},
+  updateItemQuantity: () => {}
 });
 
 export {CartContext};
